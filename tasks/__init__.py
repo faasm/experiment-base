ea4ef8358bfcf03f208974fb5565152f5d36375b
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-03-18 12:09:05 +0000

    cleanup

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,12 +2,10 @@ from invoke import Collection
 
 from . import azure
 from . import container
-from . import faasm
 from . import knative
 
 ns = Collection(
     azure,
     container,
-    faasm,
     knative,
 )
