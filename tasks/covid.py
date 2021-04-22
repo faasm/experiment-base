32eb1ce2c7207fd70b3d2b99aba6c2833a899fc6
Simon Shillaker <mail@simonshillaker.com>
2021-04-22 11:05:51 +0200

    Tidy-up

--- tasks/covid.py
+++ tasks/covid.py
@@ -2,6 +2,7 @@ from invoke import task
 from tasks.util.env import PROJ_ROOT
 from os.path import join, exists
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 
 import matplotlib
@@ -11,26 +12,17 @@ matplotlib.use("tkagg")
 RESULTS_DIR = join(PROJ_ROOT, "results")
 
 
-def _do_individual_plot(csv, label, ax):
+def read_results(csv):
+    csv = join(RESULTS_DIR, "covid", csv)
+
+    if not exists(csv):
+        raise RuntimeError("CSV not found: {}".format(native_csv))
+
     results = pd.read_csv(csv)
 
-    # Average over runs
     grouped = results.groupby("Threads")
     times = grouped.mean()
     errs = grouped.std()
-
-    times.plot.line(
-        y=["Setup", "Execution"],
-        yerr=errs,
-        ecolor="gray",
-        elinewidth=0.8,
-        capsize=1.0,
-        ax=ax,
-    )
-
-    plt.title("{} CovidSim".format(label))
-    plt.ylabel("Time (s)")
-    ax.set_ylim(bottom=0)
 
     return grouped, times, errs
 
@@ -40,33 +32,37 @@ def plot(ctx, country="Guam"):
     """
     Plot the covid results
     """
-    native_csv = join(
-        RESULTS_DIR, "covid", "covid_native_{}.csv".format(country)
+    native_csv = "covid_native_{}.csv".format(country)
+    wasm_csv = "covid_wasm_{}.csv".format(country)
+
+    native_grouped, native_times, native_errs = read_results(native_csv)
+    wasm_grouped, wasm_times, wasm_errs = read_results(wasm_csv)
+
+    ax = plt.subplot(111)
+    
+    wasm_times.plot.line(
+        y="Execution",
+        yerr=wasm_errs,
+        ecolor="gray",
+        elinewidth=0.8,
+        capsize=1.0,
+        ax=ax,
+        label="Faasm",
     )
-    wasm_csv = join(RESULTS_DIR, "covid", "covid_wasm_{}.csv".format(country))
-
-    if not exists(native_csv):
-        raise RuntimeError("Native CSV not found: {}".format(native_csv))
-
-    if not exists(wasm_csv):
-        raise RuntimeError("Wasm CSV not found: {}".format(wasm_csv))
-
-    ax = plt.subplot(311)
-    native_grouped, native_times, native_errs = _do_individual_plot(
-        native_csv, "Native", ax
-    )
-
-    ax = plt.subplot(312)
-    wasm_grouped, wasm_times, wasm_errs = _do_individual_plot(
-        wasm_csv, "Wasm", ax
-    )
-
-    # Combined plot
-    ax = plt.subplot(313)
-    wasm_times.plot.line(y="Execution", yerr=wasm_errs, ecolor="gray", ax=ax)
+   
     native_times.plot.line(
-        y="Execution", yerr=native_errs, ecolor="gray", ax=ax
+        y="Execution",
+        yerr=native_errs,
+        ecolor="gray",
+        elinewidth=0.8,
+        capsize=1.0,
+        ax=ax,
+        label="Native",
     )
+
+    tick_spacing = 1
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+
     plt.title("Combined")
     ax.set_ylim(bottom=0)
 
