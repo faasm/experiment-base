723c567f5925b7adc3ec49fdfa45d34ec76837ac
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2021-04-22 13:48:30 +0200

    Covid wasm experiment (#13)
    * Latest cov experiment
    
    * Adding plot lines for covid
    
    * Faasm covid result
    
    * Cov plot
    
    * Native and wasm covid plots
    
    * Ylim
    
    * Results
    
    * Added Guam results
    
    * More wasm plotting
    
    * Latest covid exp
    
    * Covid native malta
    
    * Covid wasm results
    
    * Tidy-up
    
    * Formatting

--- tasks/covid.py
+++ tasks/covid.py
@@ -1,31 +1,70 @@
 from invoke import task
 from tasks.util.env import PROJ_ROOT
-from os.path import join
+from os.path import join, exists
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
+
+import matplotlib
+
+matplotlib.use("tkagg")
 
 RESULTS_DIR = join(PROJ_ROOT, "results")
 
 
-@task
-def plot(ctx):
-    """
-    Plot the covid results
-    """
-    native_csv = join(RESULTS_DIR, "covid", "covid_native.csv")
-    results = pd.read_csv(native_csv)
+def read_results(csv):
+    csv = join(RESULTS_DIR, "covid", csv)
 
-    # Average over runs
+    if not exists(csv):
+        raise RuntimeError("CSV not found: {}".format(native_csv))
+
+    results = pd.read_csv(csv)
+
     grouped = results.groupby("Threads")
     times = grouped.mean()
     errs = grouped.std()
 
-    # Plot
-    times.plot.line(
-        y="Time(s)", yerr=errs, ecolor="gray", elinewidth=0.8, capsize=1.0
+    return grouped, times, errs
+
+
+@task
+def plot(ctx, country="Guam"):
+    """
+    Plot the covid results
+    """
+    native_csv = "covid_native_{}.csv".format(country)
+    wasm_csv = "covid_wasm_{}.csv".format(country)
+
+    native_grouped, native_times, native_errs = read_results(native_csv)
+    wasm_grouped, wasm_times, wasm_errs = read_results(wasm_csv)
+
+    ax = plt.subplot(111)
+
+    wasm_times.plot.line(
+        y="Execution",
+        yerr=wasm_errs,
+        ecolor="gray",
+        elinewidth=0.8,
+        capsize=1.0,
+        ax=ax,
+        label="Faasm",
     )
 
-    plt.title("CovidSim run time")
-    plt.legend(["Native"])
-    plt.ylabel("Time (s)")
+    native_times.plot.line(
+        y="Execution",
+        yerr=native_errs,
+        ecolor="gray",
+        elinewidth=0.8,
+        capsize=1.0,
+        ax=ax,
+        label="Native",
+    )
+
+    tick_spacing = 1
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+
+    plt.title("Combined")
+    ax.set_ylim(bottom=0)
+
+    plt.tight_layout()
     plt.show()
