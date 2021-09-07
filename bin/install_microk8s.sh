0397fcd7f70b42b67474d50e1e6eee6eb281d33a
Simon Shillaker <scs17@ic.ac.uk>
2021-09-07 10:25:40 +0000

    Add microk8s addons

--- bin/install_microk8s.sh
+++ bin/install_microk8s.sh
@@ -8,3 +8,8 @@ K8S_MAJOR=${K8S_VERSION:0:4}
 echo "Installing microk8s version ${K8S_MAJOR}"
 
 sudo snap install microk8s --classic --channel=${K8S_MAJOR}/stable
+
+# Enable add-ons required by faasm/ knative
+sudo microk8s enable dns
+sudo microk8s enable rbac
+sudo microk8s enable metallb:192.168.1.240/24
