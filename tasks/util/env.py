541876738f17008684ae59746860cc5203e889eb
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2022-02-02 13:24:01 +0100

    Move plots and results into experiment-specific repos (#31)
    * Move lulesh results to repo
    
    * Remove plots and results
    
    * Remove unused utils
    
    * Switch to openmp submodule
    
    * Latest MPI experiment
    
    * Latest openmp
    
    * Latest submodules

--- tasks/util/env.py
+++ tasks/util/env.py
@@ -1,13 +1,9 @@
 from os.path import dirname, realpath, join
 
 PROJ_ROOT = dirname(dirname(dirname(realpath(__file__))))
-FAASM_DIR = "{}/faasm".format(PROJ_ROOT)
 
 BIN_DIR = join(PROJ_ROOT, "bin")
 GLOBAL_BIN_DIR = "/usr/local/bin"
-
-PLOTS_ROOT = join(PROJ_ROOT, "plots")
-PLOTS_FORMAT = "pdf"
 
 AZURE_RESOURCE_GROUP = "faasm"
 AZURE_REGION = "eastus"
