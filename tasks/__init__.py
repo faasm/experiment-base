0956dd18fd3e90212360bc53bdf275abd5a6a851
Simon Shillaker <mail@simonshillaker.com>
2022-02-02 10:49:20 +0000

    Remove plots and results

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,22 +1,12 @@
 from invoke import Collection
 
 from . import cluster
-from . import covid
-from . import kernels
-from . import lammps
-from . import lulesh
-from . import migration
 from . import monitor
 from . import storage
 from . import uk8s
 
 ns = Collection(
     cluster,
-    covid,
-    kernels,
-    lammps,
-    lulesh,
-    migration,
     monitor,
     storage,
     uk8s,
