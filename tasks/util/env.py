4403749cbf9bbd63f10cb7749f5a738240d7947a
Simon Shillaker <mail@simonshillaker.com>
2022-02-25 14:36:33 +0100

    Revert region change

--- tasks/util/env.py
+++ tasks/util/env.py
@@ -6,7 +6,7 @@ BIN_DIR = join(PROJ_ROOT, "bin")
 GLOBAL_BIN_DIR = "/usr/local/bin"
 
 AZURE_RESOURCE_GROUP = "faasm"
-AZURE_REGION = "uksouth"
+AZURE_REGION = "eastus"
 
 AZURE_STORAGE_SKU = "Standard_LRS"
 AKS_CLUSTER_NAME = "faasm-cluster"
