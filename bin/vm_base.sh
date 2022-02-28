6fce5ab2e6deb6300a69c9f4f45eac24e3eea090
Simon Shillaker <mail@simonshillaker.com>
2022-02-28 15:45:50 +0100

    Fix up base script

--- bin/vm_base.sh
+++ bin/vm_base.sh
@@ -14,23 +14,27 @@ sudo apt update
 sudo apt upgrade -y
 
 # Code
-mkdir ~/code
-cd ~/code
+mkdir -p ~/code
+pushd ~/code
 
+export GIT_SSL_NO_VERIFY=true
 git clone git@github.com:faasm/faasm.git
-cd faasm
+pushd faasm
 git submodule update --init
 
+popd
+popd
+
 # HWE
-sudo apt install --install-recommends linux-generic-hwe-20.04
+sudo apt install -y --install-recommends linux-generic-hwe-20.04
 
 # Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 sudo apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 # Docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -38,7 +42,6 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 # Add user to docker group
 sudo usermod -aG docker ${USER}
-su - ${USER}
 
 # Done
 echo ""
