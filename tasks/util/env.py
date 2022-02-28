85122639c1eb59a185c7f763e62049e7d2876528
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2022-02-28 15:59:18 +0100

    SGX VMs and generic setup script (#38)
    * Add VM deallocate and start
    
    * Add VM setup script
    
    * Fix delete-all
    
    * Add Azure VM provisioning
    
    * Merge in SGX VM stuff
    
    * Fix network component deletion
    
    * Missing constants, print OS disk cmd
    
    * Fix up base script
    
    * Tidy up SGX VM creation

--- tasks/util/env.py
+++ tasks/util/env.py
@@ -19,21 +19,31 @@ AZURE_SSH_KEY = "~/.ssh/id_rsa"
 # ----------------------------
 # VM SIZING NOTES
 #
-# For hacking around, the standard A-series VMs are fine.
+# For hacking around, the standard A-series VMs are fine (although very slow)
 # https://docs.microsoft.com/en-us/azure/virtual-machines/av2-series
 #
 # e.g. Standard_A8_v2: 8 cores, 16GB mem
 #
-# For single VM benchmarks and cluster experiments, the D-series machines are
-# more appropriate as they use higher grade CPUs.
+# For something a little faster, e.g. testing out experiment configurations,
+# a smaller D-series VM works:
+#
+# e.g. Standard_D8_v5: 8 cores, 28GB mem
+#
+# For single VM benchmarks and cluster experiments, bigger D-series machines
+# are more appropriate (but more expensive).
 # https://docs.microsoft.com/en-us/azure/virtual-machines/dv3-dsv3-series
 #
-# e.g. Standard_D16_v2: 16 cores, 64GB mem
-# e.g. Standard_D32_v3: 32 cores, 128GB mem
+# e.g. Standard_D16_v5: 16 cores, 64GB mem
+# e.g. Standard_D32_v5: 32 cores, 128GB mem
+#
+# ---------------------------
 
 AZURE_VM_ADMIN = "faasm"
 AZURE_VM_IMAGE = "Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest"
 
-AZURE_STANDALONE_VM_SIZE = "Standard_A8_v2"
+AZURE_STANDALONE_VM_SIZE = "Standard_D8_v5"
 AZURE_K8S_VM_SIZE = "Standard_D16_v5"
 AKS_CLUSTER_NODE_COUNT = 4
+
+AZURE_SGX_VM_IMAGE = "Canonical:UbuntuServer:18_04-lts-gen2:18.04.202109180"
+AZURE_SGX_VM_SIZE = "Standard_DC2ds_v3"
--- tasks/vm.py
+++ tasks/vm.py
@@ -11,52 +11,32 @@ from tasks.util.env import (
     AZURE_VM_ADMIN,
     AZURE_VM_IMAGE,
     AZURE_STANDALONE_VM_SIZE,
+    AZURE_SGX_VM_IMAGE,
+    AZURE_SGX_VM_SIZE,
 )
 
+# Network components to be deleted, order matters
+VM_NET_COMPONENTS = [
+    ("nic", "VMNic"),
+    ("nsg", "NSG"),
+    ("vnet", "VNET"),
+    ("public-ip", "PublicIp"),
+]
 
-@task
-def create(ctx, region=AZURE_REGION):
-    """
-    Creates a single Azure VM
-    """
 
-    timestamp = datetime.now().strftime("%d%m%Y-%H%M%S")
-    name = "faasm-vm-{}".format(timestamp)
-
-    print(
-        "Creating VM {}, size {}, region {}".format(
-            name, AZURE_STANDALONE_VM_SIZE, region
-        )
-    )
-
+def _get_ip(name):
     cmd = [
-        "az vm create",
-        "--resource-group {}".format(AZURE_RESOURCE_GROUP),
-        "--name {}".format(name),
-        "--image {}".format(AZURE_VM_IMAGE),
-        "--admin-username {}".format(AZURE_VM_ADMIN),
-        "--size {}".format(AZURE_STANDALONE_VM_SIZE),
-        "--ssh-key-value {}".format(AZURE_PUB_SSH_KEY),
-        "--location {}".format(region),
+        "az vm list-ip-addresses",
+        "-n {}".format(name),
+        "-g {}".format(AZURE_RESOURCE_GROUP),
     ]
+
     cmd = " ".join(cmd)
-    print(cmd)
+    res = run(cmd, shell=True, check=True, stdout=PIPE, stderr=STDOUT)
 
-    res = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    if res.returncode == 0:
-        print(res.stdout)
-        res = json.loads(res.stdout)
-
-        print("\nTo SSH:")
-        print(
-            "ssh -A -i {} {}@{}".format(
-                AZURE_SSH_KEY, AZURE_VM_ADMIN, res["publicIpAddress"]
-            )
-        )
-    else:
-        print(res.stderr)
-        print(res.stdout)
-        raise RuntimeError("Failed to provision VM")
+    res = json.loads(res.stdout)
+    vm_info = res[0]["virtualMachine"]
+    return vm_info["network"]["publicIpAddresses"][0]["ipAddress"]
 
 
 def _list_all_vms():
@@ -74,18 +54,124 @@ def _list_all_vms():
     return res
 
 
-def _delete_vm(name):
-    print("Deleting VM {}".format(name))
+def _vm_op(op, name, extra_args=None, capture=False):
+    print("Performing {} on {}".format(op, name))
 
     cmd = [
-        "az vm delete",
+        "az vm {}".format(op),
         "--resource-group {}".format(AZURE_RESOURCE_GROUP),
         "--name {}".format(name),
-        "--yes",
     ]
+
+    if extra_args:
+        cmd.extend(extra_args)
+
     cmd = " ".join(cmd)
     print(cmd)
-    run(cmd, shell=True, check=True)
+
+    if capture:
+        res = run(cmd, shell=True, check=True, stdout=PIPE, stderr=PIPE)
+        return res.stdout.decode("utf-8")
+    else:
+        run(cmd, shell=True, check=True)
+
+
+def _build_ssh_command(ip_addr):
+    return "ssh -A -i {} {}@{}".format(AZURE_SSH_KEY, AZURE_VM_ADMIN, ip_addr)
+
+
+@task
+def create(ctx, region=AZURE_REGION, sgx=False, name=None):
+    """
+    Creates a single Azure VM
+    """
+    if not name:
+        timestamp = datetime.now().strftime("%d%m%Y-%H%M%S")
+        name = "faasm-vm{}-{}".format("-sgx" if sgx else "", timestamp)
+
+    print(
+        "Creating VM {}, size {}, region {}".format(
+            name, AZURE_STANDALONE_VM_SIZE, region
+        )
+    )
+
+    cmd = [
+        "az vm create",
+        "--resource-group {}".format(AZURE_RESOURCE_GROUP),
+        "--name {}".format(name),
+        "--admin-username {}".format(AZURE_VM_ADMIN),
+        "--location {}".format(region),
+        "--ssh-key-value {}".format(AZURE_PUB_SSH_KEY),
+    ]
+
+    if sgx:
+        cmd.extend(
+            [
+                "--image {}".format(AZURE_SGX_VM_IMAGE),
+                "--size {}".format(AZURE_SGX_VM_SIZE),
+            ]
+        )
+    else:
+        cmd.extend(
+            [
+                "--image {}".format(AZURE_VM_IMAGE),
+                "--size {}".format(AZURE_STANDALONE_VM_SIZE),
+            ]
+        )
+
+    cmd = " ".join(cmd)
+    print(cmd)
+
+    res = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    if res.returncode == 0:
+        res = json.loads(res.stdout)
+
+        print("\nTo SSH:")
+        print(_build_ssh_command(res["publicIpAddress"]))
+    else:
+        print(res.stderr)
+        print(res.stdout)
+        raise RuntimeError("Failed to provision VM")
+
+
+@task
+def ssh(ctx, name):
+    """
+    Prints SSH information for given VM
+    """
+    ip_addr = _get_ip(name)
+    print("--- SSH command ---\n")
+    print(_build_ssh_command(ip_addr))
+
+    print("\n--- SSH config ---")
+    print(
+        """
+# Faasm SGX VM
+Host {}
+HostName {}
+User {}
+ForwardAgent yes
+        """.format(
+            name, ip_addr, AZURE_VM_ADMIN
+        )
+    )
+
+
+@task
+def start(ctx, name):
+    """
+    Starts (powers on) the given Azure VM.
+    """
+    _vm_op("start", name, ["--no-wait"])
+
+
+@task
+def deallocate(ctx, name):
+    """
+    Deallocates, i.e. powers down and deallocates compute resource for the
+    given Azure VM so that it's not billed.
+    """
+    _vm_op("stop", name)
 
 
 @task
@@ -93,7 +179,49 @@ def delete(ctx, name):
     """
     Deletes the given Azure VM
     """
-    _delete_vm(name)
+    # Get OS disk name
+    disk_out = _vm_op(
+        "show",
+        name,
+        [
+            "--query storageProfile.osDisk.managedDisk.id",
+        ],
+        capture=True,
+    )
+    os_disk = disk_out.split("/")[-1][:-2]
+
+    # Delete the VM itself
+    _vm_op("delete", name, ["--yes"])
+
+    # Delete OS disk
+    delete_disk_cmd = [
+        "az disk delete",
+        "--resource-group {}".format(AZURE_RESOURCE_GROUP),
+        "--name {}".format(os_disk),
+        "--yes",
+    ]
+
+    delete_disk_cmd = " ".join(delete_disk_cmd)
+    print(delete_disk_cmd)
+    run(
+        delete_disk_cmd,
+        check=True,
+        shell=True,
+    )
+
+    # Delete all the network components
+    for component, suffix in VM_NET_COMPONENTS:
+        cmd = [
+            "az",
+            "network {}".format(component),
+            "delete",
+            "--resource-group {}".format(AZURE_RESOURCE_GROUP),
+            "--name {}{}".format(name, suffix),
+        ]
+
+        cmd = " ".join(cmd)
+        print(cmd)
+        run(cmd, shell=True, check=True)
 
 
 @task
@@ -103,7 +231,7 @@ def delete_all(ctx):
     """
     res = _list_all_vms()
     for vm in res:
-        _delete_vm(vm["name"])
+        _vm_op("delete", vm["name"], ["--yes"])
 
 
 @task
@@ -127,16 +255,5 @@ def ip(ctx, name):
     """
     Show the IP details of a given VM
     """
-
-    cmd = [
-        "az vm list-ip-addresses",
-        "-n {}".format(name),
-        "-g {}".format(AZURE_RESOURCE_GROUP),
-    ]
-
-    cmd = " ".join(cmd)
-    res = run(cmd, shell=True, check=True, stdout=PIPE, stderr=STDOUT)
-
-    res = json.loads(res.stdout)
-    vm_info = res[0]["virtualMachine"]
-    print(vm_info["network"]["publicIpAddresses"][0]["ipAddress"])
+    ip = _get_ip()
+    print(ip)
